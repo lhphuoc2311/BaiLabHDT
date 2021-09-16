6defeea1825f90e87e2f6cd684f62495ce85badf
Code Guru Mouse <phuoc2311@gmail.com>
2021-09-16 11:56:27 +0700

    bài tập 1 lab 1

---
+++ src/vn/edu/giadinh/tuan02/Bai1Lab1.java
@@ -0,0 +1,13 @@
+/*
+*  crated date: 9/16/2021
+*  author: cgm
+*/
+package vn.edu.giadinh.tuan02;
+
+public class Bai1Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Triên khai code để làm bài tập ....");
+    }
+    
+}
