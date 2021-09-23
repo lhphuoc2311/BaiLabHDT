c518a1d0ab9cdbae7e93d302576156f2b6d38584
Code Guru Mouse <phuoc2311@gmail.com>
2021-09-23 10:54:36 +0700

    cập nhật số nguyên tố lab3

--- src/com/phuoclh123456/lab3/SoNguyenTo.java
+++ src/com/phuoclh123456/lab3/SoNguyenTo.java
@@ -4,10 +4,23 @@
 */
 package com.phuoclh123456.lab3;
 
+import java.util.Scanner;
+
 public class SoNguyenTo {
 
     public static void main(String[] args) {
         //bài này giải sao ta???
+        boolean ok = true;
+
+        Scanner bienNhap = new Scanner(System.in);
+
+        System.out.print("Nhập vào 1 số nguyên: ");
+
+        int N = bienNhap.nextInt();
+
+        for(int i = 2; i < N - 1; i ++){
+
+        }
     }
 
     
