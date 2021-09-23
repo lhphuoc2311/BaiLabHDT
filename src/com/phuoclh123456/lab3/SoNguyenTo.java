8d75bc345e3b7a9dfcf3b75fef7dd8766f255db9
Code Guru Mouse <phuoc2311@gmail.com>
2021-09-23 10:46:05 +0700

    thêm bài số nguyên tố lab3

---
+++ src/com/phuoclh123456/lab3/SoNguyenTo.java
@@ -0,0 +1,14 @@
+/*
+*  crated date: Sep 23, 2021
+*  author: cgm
+*/
+package com.phuoclh123456.lab3;
+
+public class SoNguyenTo {
+
+    public static void main(String[] args) {
+        //bài này giải sao ta???
+    }
+
+    
+}
